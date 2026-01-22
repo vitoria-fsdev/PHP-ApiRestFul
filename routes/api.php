d76569cce37cd667a8eb99de74a597e042d58f17
rannyzita <fs.vitoria.soares@gmail.com>
2026-01-22 16:46:54 -0300

    feat: adiciona a rota de buscar todos os usuarios -> '/users'

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/users', function(Request $request) {
+    return User::all(); 
+});
